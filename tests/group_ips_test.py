93c2d5c5725956dbfd0d1bb8beda3b7df85be83d
Ryan Peck <ryan@rypeck.com>
2014-02-25 01:53:02 -0500

    Preliminary IPv6 support.
    Very little difference between the two. Had to modify more lines for the
    test than I had to to implement the function.
    
    TODO - Add more testing for IPv6.

--- tests/group_ips_test.py
+++ tests/group_ips_test.py
@@ -25,7 +25,7 @@ class TestGroupIPs(unittest.TestCase):
                             "127.2.0.0/16": 2,
                             }
 
-        group = groupips.group_IPs(IPs, 16)
+        group = groupips.group_IPv4s(IPs, 16)
 
         self.assertEqual(expected_results, group)
 
@@ -46,14 +46,14 @@ class TestGroupIPs(unittest.TestCase):
                             "127.2.1.0/24": 1,
                             }
 
-        group = groupips.group_IPs(IPs, 24)
+        group = groupips.group_IPv4s(IPs, 24)
 
         self.assertEqual(expected_results, group)
 
     def test_group3(self):
         """ 'Random' test """
 
-        # Small Netblock so we don't do over 2**22 hosts to test with
+        # Small Netblock so we don't do over 2**10 hosts to test with
         random_cidr = random.randint(22, 30)
 
         network = ipaddress.IPv4Network(("129.21.0.0/%s" % random_cidr))
@@ -65,7 +65,28 @@ class TestGroupIPs(unittest.TestCase):
 
         expected_results = {("129.21.0.0/%s" % random_cidr): random_int}
 
-        group = groupips.group_IPs(IPs, random_cidr)
+        group = groupips.group_IPv4s(IPs, random_cidr)
+
+        self.assertEqual(expected_results, group)
+
+    def test_IPv6(self):
+        """ 'Random' test """
+
+        # Small Netblock so we don't do over 2**10 hosts to test with
+        random_cidr = random.randint(118, 126)
+
+        network = ipaddress.IPv6Network(("2607:f8b0:4009:803::/%s" %
+                                        random_cidr))
+
+        # So out sample size is never bigger than the population of hosts
+        random_int = random.randint(1, 2**(128 - random_cidr - 1))
+
+        IPs = random.sample(set(network.hosts()), random_int)
+
+        expected_results = {("2607:f8b0:4009:803::/%s" % random_cidr):
+                            random_int}
+
+        group = groupips.group_IPv6s(IPs, random_cidr)
 
         self.assertEqual(expected_results, group)
 
