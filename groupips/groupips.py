fef6dca3e80aa20367bfc9f3a6373f9f2a3fb270
Ryan Peck <ryan@rypeck.com>
2014-04-03 03:15:46 -0400

    TotalAddresses working with no network overlap
    Function is now working when the inputted networks don't overlap at all
    
    [ci skip]

--- groupips/groupips.py
+++ groupips/groupips.py
@@ -32,3 +32,77 @@ def group_IPv4s(ips, bits):
 
     ip_objs = [ipaddress.IPv4Network(i) for i in ips]
     return _group_IPs(ip_objs, bits)
+
+
+def totalAddresses(ips):
+    ips = listify_params(ips)
+    print(ips)
+
+    total = 0
+
+    for i in ips:
+        total += i.num_addresses
+
+    return total
+
+
+def listify_params(args):
+    """
+    Create a list of IP Network Objects from parameters, must be either IPv4
+    or IPv6...
+    """
+    assert(validate_ips_param(args))
+
+    ipv4_bool = False
+    ipv6_bool = False
+
+    if isinstance(args, str):
+        args = [ipaddress.ip_network(args, strict=False)]
+
+    new_args = []
+
+    for i in args:
+        n = ipaddress.ip_network(i, strict=False)
+
+        if isinstance(n, ipaddress.IPv4Network):
+            ipv4_bool = True
+        if isinstance(n, ipaddress.IPv6Network):
+            ipv6_bool = True
+
+        # Can't have both types in a list...
+        assert(ipv6_bool ^ ipv4_bool)
+
+        new_args.append(n)
+
+    return new_args
+
+
+# TODO Write tests for this
+def validate_ips_param(ips):
+    """
+    Validate that the parameters passed are types we accept.
+    """
+
+    # Acceptable inputs
+    assert(isinstance(ips, (str, list, ipaddress.IPv4Network)))
+
+    # Unpack a list
+    if isinstance(ips, list):
+        for i in ips:
+            assert(isinstance(i, (str, ipaddress._BaseNetwork)))
+
+            if isinstance(i, str):
+                assert(validate_IPNetwork_str(i))
+
+    return True
+
+
+# TODO Write tests for this
+# Should use ipaddress.ip_network here
+def validate_IPNetwork_str(string):
+    """ Validate that a valid IP Network string was passed """
+    if isinstance(string, str):
+        temp = ipaddress.ip_network(string, strict=False)
+        del temp
+
+    return True
