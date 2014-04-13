5cfcd9cfb98cd69e6cde86afc25d15abce304607
Ryan Peck <ryan@rypeck.com>
2014-04-13 11:11:04 -0400

    Start of IP Grouping Class...
    Going in a more object oriented direction, like the IP Address module.
    
    [ci skip]

--- groupips/groupips.py
+++ groupips/groupips.py
@@ -4,22 +4,33 @@ import collections
 import ipaddress
 
 
-def _group_IPs(ip_objs, bits):
-    """ Group IPs by the bits that match """
+class _BaseGroup():
 
-    super_nets = set([i.supernet(new_prefix=bits) for i in ip_objs])
+    """A generic group of IP Addresses/Networks
 
-    group = collections.defaultdict(int)
+    This class will containt version indepenent methods for grouping.
 
-    while ip_objs != []:
-        n = ip_objs.pop()
-        for x in super_nets:
-            if x.overlaps(n):
-                group[str(x)] += 1
-                break
+    """
 
-    # Return it to a normal dictionary
-    return dict(group)
+    def __init__(self, ip_objs, bits):
+        pass
+
+    def _group_IPs(ip_objs, bits):
+        """ Group IPs by the bits that match """
+
+        super_nets = set([i.supernet(new_prefix=bits) for i in ip_objs])
+
+        group = collections.defaultdict(int)
+
+        while ip_objs != []:
+            n = ip_objs.pop()
+            for x in super_nets:
+                if x.overlaps(n):
+                    group[str(x)] += 1
+                    break
+
+        # Return it to a normal dictionary
+        return dict(group)
 
 
 def group_IPv6s(ips, bits):
