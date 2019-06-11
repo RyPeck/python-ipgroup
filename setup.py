b8df7fdd6f63f4daf7220875f2656c167856f9ff
Ryan John Peck <iamreck@gmail.com>
2019-06-11 13:24:31 -0400

    Update Python Versions

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright 2014 Ryan Peck
+# Copyright 2019 Ryan Peck
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,28 +20,30 @@ import ipgroup
 
 
 setup(
-    name='ipgroup',
-    maintainer='Ryan Peck',
-    maintainer_email='ryan@rypeck.com',
+    name="ipgroup",
+    maintainer="Ryan Peck",
+    maintainer_email="ryan@rypeck.com",
     version=ipgroup.__version__,
-    url='https://github.com/RyPeck/python-ipgroup',
-    license='Apache License, Version 2.0',
+    url="https://github.com/RyPeck/python-ipgroup",
+    license="Apache License, Version 2.0",
     classifiers=[
-        'Development Status :: 1 - Planning',
-        'Intended Audience :: Developers',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Internet',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: System :: Networking',
-        ],
-    description='Functions to gather info on a group of IPv4 or IPv6 Networks',
-    py_modules=['ipgroup'],
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: Telecommunications Industry",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Internet",
+        "Topic :: Internet :: Log Analysis",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: System :: Networking",
+        "Topic :: Utilities",
+    ],
+    description="Functions to gather info on a group of IPv4 or IPv6 Networks",
+    py_modules=["ipgroup"],
 )
